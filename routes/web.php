546c67ee4698ce2e0cc2b951ed15c7b5171c6fcf
Joe Rylander <rylander.j92@gmail.com>
2025-08-15 10:05:50 +0800

    Add Auth and Dashboard controllers; implement login view and update dashboard view structure

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\NinjaController;
 
 Route::get('/', function () {
@@ -12,3 +14,7 @@ Route::get("/ninjas/create", [NinjaController::class, 'create'])->name('ninjas.c
 Route::get("/ninjas/{ninja}", [NinjaController::class, 'show'])->name('ninjas.show');
 Route::post("/ninjas", [NinjaController::class, 'store'])->name('ninjas.store');
 Route::delete("/ninjas/{ninja}", [NinjaController::class, 'destroy'])->name('ninjas.destroy');
+
+Route::get('/auth/login', [AuthController::class, 'index'])->name('auth.index');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
